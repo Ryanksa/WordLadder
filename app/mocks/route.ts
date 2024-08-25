f4538e18035605a31ffe91096b5ed039ca689c3c
Ryanksa <ryan@hiboomerang.com>
2024-08-24 20:57:08 -0400

    oops forgot to test build

--- app/mocks/route.ts
+++ app/mocks/route.ts
@@ -1,7 +1,14 @@
 // TODO: temporary mock EP while I work on FE, remove after BE is ready
 export async function GET(_request: Request) {
-  return Response.json({
-    start: "BONG",
-    end: "DOOM",
-  });
+  return new Response(
+    JSON.stringify({
+      start: "BONG",
+      end: "DOOM",
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
